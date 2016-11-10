f7c5228f5de89c8be748c6f4ba0f5176c85c2a31
Drake Ballard <drakeballard@gmail.com>
2016-11-10 17:41:28 -0600

    adding some cdn into the survey file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-//route files
+//route files through the different channels
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
