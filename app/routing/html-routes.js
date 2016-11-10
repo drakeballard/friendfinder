8d3b270895cbe38863fe2e2254ef451c3d553c18
Drake Ballard <drakeballard@gmail.com>
2016-11-10 17:08:30 -0600

    adding the html routing

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,3 +2,21 @@
 var path = require('path');
 
 // seting a module export section that will switch back from home and the survey section
+module.exports = function(app) {
+
+	// the home directory
+	app.get('/home', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	//the surve directory
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	//goes back to the home
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+};
