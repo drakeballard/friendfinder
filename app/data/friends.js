bb716b07ac77cf3b32357ef66ac3c9735f2307c2
Drake Ballard <drakeballard@gmail.com>
2016-11-12 23:07:20 -0600

    adding the framework in the friends.js file to be able to run a comparion

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// setting a value for the list of comparison
+var friendFindComparison = [
+  {
+    'name':'',
+    'picture':'',
+    'score':[''],
+
+  }
+]
+module.exports = friendFindComparison;
