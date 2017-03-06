cc8599241c4a1f04bb0d9d643736004d6b48d4a4
Drake Ballard <drakeballard@gmail.com>
2017-03-06 14:13:20 -0600

    adding different characters into the friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 // setting a value for the list of comparison
 var friendFindComparison = [
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Tony Stark",
+    picture:"https://www.xda-developers.com/files/2016/03/Robert-Downey-Jr-in-Iron-Man-2-Armor.jpg",
     score:[
       "",
       "",
@@ -18,26 +18,8 @@ var friendFindComparison = [
 
   },
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
-    score:[
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      ""
-    ]
-
-  },
-
-  {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Steve Rogers",
+    picture:"http://i.annihil.us/u/prod/marvel/movies/civilwar/images/captainamerica_hero.png",
     score:[
       "",
       "",
@@ -54,8 +36,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Bruce Banner",
+    picture:"http://img.cinemablend.com/cb/3/f/e/e/3/7/3fee37dcfa24d7df1eb34eeddd051cf47e6cb0151d82ec2c88c9331f4ff6cd1a.jpg",
     score:[
       "",
       "",
@@ -72,8 +54,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Thor",
+    picture:"http://img.cinemablend.com/quill/b/e/e/0/e/f/bee0ef8beb8cc11ced998fc20ec49c128671dc1d.jpg",
     score:[
       "",
       "",
@@ -90,8 +72,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Scott Lang",
+    picture:"http://turntherightcorner.com/wp-content/uploads/2015/04/ant-man-movie-screenshot-scott-lang-paul-rudd-suit-21.jpg",
     score:[
       "",
       "",
@@ -108,8 +90,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Peter Parker",
+    picture:"http://wide-wallpapers.net/download/peter-parker-wide-wallpaper-1280x800.jpg",
     score:[
       "",
       "",
@@ -126,8 +108,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Nick Fury",
+    picture:"https://images.moviepilot.com/image/upload/c_fill,h_470,q_auto:good,w_620/tmpmjgnbpmlu1djodtv4.jpg",
     score:[
       "",
       "",
@@ -144,8 +126,8 @@ var friendFindComparison = [
   },
 
   {
-    name:"Tony",
-    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    name:"Natasha Romanoff",
+    picture:"https://static1.squarespace.com/static/53323bb4e4b0cebc6a28ffa2/t/57ff8de4e6f2e1a3ef801994/1476365802937/?format=750w",
     score:[
       "",
       "",
@@ -159,23 +141,25 @@ var friendFindComparison = [
       ""
     ]
 
-  },  {
-      name:"Tony",
-      picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
-      score:[
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
-      ]
+  },
 
-    },
+  {
+    name:"Clint Barton",
+    picture:"http://static2.comicvine.com/uploads/original/11/113509/4411572-hawkeye+2.jpg",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
 
 ]
 module.exports = friendFindComparison;
