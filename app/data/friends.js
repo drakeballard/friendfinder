375a29f14d58235ba832bcbb3771758daedcd6bc
Drake Ballard <drakeballard@gmail.com>
2017-03-06 14:03:54 -0600

    working on the friends.js file under app and creating the blueprint. Information needs to be included with the remainder 9

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,181 @@
 // setting a value for the list of comparison
 var friendFindComparison = [
   {
-    'name':'Tony',
-    'picture':'http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603',
-    'score':[
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      ''
-    ],
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
 
-  }
+  },
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+
+  {
+    name:"Tony",
+    picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+    score:[
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },  {
+      name:"Tony",
+      picture:"http://vignette2.wikia.nocookie.net/marvelcentral/images/9/97/Tony-Stark.jpg/revision/latest?cb=20130429010603",
+      score:[
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+
+    },
+
 ]
 module.exports = friendFindComparison;
